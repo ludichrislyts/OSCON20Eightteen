c0e1fab396f66b33a34aba5794a038f91d586523
Matthew Larson <matthewmatician@gmail.com>
2018-04-02 14:59:32 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <p>Hello World.</p>
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import reducer from './reducers';
+
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}><App/></Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
