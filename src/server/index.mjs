596465fd38547d0f063338f186ec9ff38bca5ece
Matthew Larson <matthewmatician@gmail.com>
2018-06-12 20:19:52 -0600

    Time is now streaming.

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,6 +1,6 @@
 /* eslint-env: node */
 import WebSocket from 'ws';
-import { newConnection } from './sessions.mjs';
+import { newConnection, broadcast } from './sessions.mjs';
 import reducer from '../reducers/index.mjs';
 import createStore from './liteStore.mjs';
 import actions from '../actions/index.mjs';
@@ -11,6 +11,15 @@ store.dispatch(actions.board.load([
   [[0, 0], [500, 0], [500, 500], [0, 500], [0, 0]],
   [[150, 150], [350, 350]],
 ]));
+
+let now = Date.now();
+setInterval(() => {
+  const future = Date.now();
+  const action = actions.time(future - now);
+  store.dispatch(action);
+  broadcast(action);
+  now = future;
+}, 10);
 
 const server = new WebSocket.Server({ port });
 console.log(`Server listening on port ${port}...`);
